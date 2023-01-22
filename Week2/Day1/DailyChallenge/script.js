32077504c2e90ec8fff4757678af6e9374f30e6a
shlonimsky <ashlonimsky@gmail.com>
2023-01-22 15:18:26 +0200

    adding daily ch

---
+++ Week2/Day1/DailyChallenge/script.js
@@ -0,0 +1,22 @@
+// Exercise 1
+// // Using this array :
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// Remove Banana from the array.
+// Sort the array in alphabetical order.
+// Add “Kiwi” to the end of the array.
+// Remove “Apples” from the array. Don’t use the same method as in part 1.
+// Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// At the end you should see this outcome: ["Kiwi", "Oranges", "Blueberries"]
+
+fruits.splice(0,1)
+fruits.sort();
+fruits.push('Kiwi')
+fruits.shift()
+fruits.reverse()
+console.log(fruits)
+
+//Exercise 2:
+// Using this array :
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// Access and then console.log “Oranges”. 
+console.log(moreFruits[1][1][0]);
