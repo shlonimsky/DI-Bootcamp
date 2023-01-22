ae6cd5bc11624a76fdfdbcfb357e6bc7deee8841
shlonimsky <ashlonimsky@gmail.com>
2023-01-22 22:02:02 +0200

    day1 ninja 1 and 2 exs

---
+++ Week2/Day1/ExercisesXPNinja/script.js
@@ -0,0 +1,62 @@
+5 >= 1
+// true
+0 === 1
+// false
+4 <= 1
+// false
+1 != 1
+// false
+"A" > "B"
+// false
+"B" < "C"
+// true
+"a" > "A"
+// NaN. The correct answer is true, because of ASCII Code. 
+// First goes A,B,C... then a,b,c. so, 0097>0065
+"b" < "A"
+// false because 0098>0065
+true === false
+// false
+true != true
+// false
+
+// Exercise 2 : Ask For Numbers
+// Ask the user for a string of numbers separated by commas
+// Console.log the sum of the numbers.
+// Hint: use some string methods
+// let inputData = prompt('please,enter a string of numbers separated by commas');
+// // let numbers = inputData.split(/,/g);
+// let var1 = Number(inputData);
+// console.log(var1);
+
+
+// Exercise 3 : Find Nemo
+// Find the word “Nemo”
+// Console.log a string as follows: "I found Nemo at [the position of the word Nemo]".
+// If you can’t find Nemo, console.log “I can’t find Nemo”.
+// let sentence = prompt(`Please, write a sentence containing the word “Nemo”. For example "I love the movie named Nemo"`);
+// sentence = sentence.toLowerCase()
+// sentence=sentence.toString()
+// let sentenceArray = sentence.split(',');
+// let found = sentenceArray[0].indexOf('nemo');
+// console.log(sentence)
+
+// Exercise 4 : Boom
+// Prompt the user for a number. Depending on the users number you will return a string of the word “Boom”. Follow the rules below:
+
+// If the number given is less than 2 : return “boom”
+// If the number given is bigger than 2 : the string should include n number of “o”s (n being the number given)
+// If the number given is evenly divisible by 2, add a exclamation mark to the end.
+// If the number given is evenly divisible by 5, return the string in ALL CAPS.
+// If the number given is evenly divisible by both 2 and 5, return the string in ALL CAPS and add an exclamation mark to the end.
+
+const num = prompt(`Please, enter a number:`);
+let final;
+if (num < 2) {
+ final = 'Boom'
+} else if (num > 2){
+    let num1 = 'o';
+     final = `B${num1.padStart(num,'o')}m`
+} else if(num % 2 == 0){
+    final = final+'!'
+}
---
+++ Week2/Day2/ExercisesXP/script.js
@@ -0,0 +1,54 @@
+// Exercise 1: Simple If/Else Statement
+// Create 2 variables, x and y. Each of them should have a different numeric value.
+// Write an if/else statement that checks which number is bigger.
+let x = 5;
+let y = 8;
+if (x > y){
+    console.log(`x is the biggest number`)
+} else {
+    console.log(`y is the biggest number`)
+}
+
+// Exercise 2: Chihuahua
+// Create a variable called newDog where it’s value is “Chihuahua”.
+// Check and display how many letters are in newDog.
+// Display the newDog variable in uppercase and then in lowercase (no need to create new variables, just console.log twice).
+// Check if the variable newDog is equal to “Chihuahua”
+// if it does, display ‘I love Chihuahuas, it’s my favorite dog breed’
+// else, console.log ‘I dont care, I prefer cats’
+let newDog = 'Chihuahua';
+console.log(newDog.length);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLocaleLowerCase());
+if (newDog == 'Chihuahua') {
+    console.log(`I love Chihuahuas, it's my favorite dog breed`)
+} else {
+    console.log(`I dont care, I prefer cats`)
+}
+
+// Exercise 3: Even Or Odd
+// Prompt the user for a number and save it to a variable.
+// Check whether the variable is even or odd.
+// If it is even, display: “x is an even number”. Where x is the actual number the user chose.
+// If it is odd, display: “x is an odd number”. Where x is the actual number the user chose.
+let number = prompt(`please, enter a number`);
+if (number % 2 == 0) {
+    console.log(`${number} is an even number`)
+} else {
+    console.log(`${number} is an odd number`)
+}
+
+// Exercise 4: Group Chat
+// Below is an array of users that are online in a group chat.
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
+// Using the array above, console.log the number of users that are connected to the group chat based on the following rules:
+// If there is no users (the users array is empty), console.log “no one is online”.
+// If there is 1 user, console.log “<name_user> is online”.
+// If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
+// If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+switch (users) {
+    case ("Lea123"):
+    let online = users[0];
+}
+console.log(online)
