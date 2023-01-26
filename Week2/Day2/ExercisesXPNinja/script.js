6d3aa0406bf6a2528abf4a49e8d0da31419a669f
shlonimsky <ashlonimsky@gmail.com>
2023-01-26 17:57:04 +0200

    other and ninja day2

--- Week2/Day2/ExercisesXPNinja/script.js
+++ Week2/Day2/ExercisesXPNinja/script.js
@@ -5,12 +5,12 @@
 const age1 = prompt(`Enter the first date of birth in format YYYY:`);
 const age2 = prompt(`Enter the first date of birth in format YYYY:`);
 let dif = Math.abs((+age1)-(+age2));
-console.log(`When the younger will be ${dif}, he will be halg age ot the older one`)
+console.log(`When the younger will be ${dif}, he will be half age ot the older one`)
 
 
 // Exercise 2 : Zip Codes
-// Harder exercise
-// Hint : This exercise has 2 parts. First, do this exercise without Regular Expressions, then do it using Regular Expressions
+// Hint : This exercise has 2 parts. First, do this exercise without 
+// Regular Expressions, then do it using Regular Expressions
 
 // While working in a Post Office you must have the clients’ zip code in order to send them their letters.
 // Ask the client for their zip code and console.log “success” or “error” based on the following rules.
@@ -18,4 +18,21 @@ console.log(`When the younger will be ${dif}, he will be halg age ot the older o
 // Must only contain numbers
 // Must not contain any whitespace (spaces)
 // Must not be greater than 5 digits in length
-const zip = prompt(`Enter zip-code of 5 numbers`);
+// Without RegExp
+let zip = prompt(`Enter zip-code of 5 numbers`);
+let zipLength = zip.length;
+zip = +zip;
+ if (zipLength === 5 && !isNaN(zip) && zip) {
+    alert(`Your zip-code was successfully received`)
+} else {
+    alert(`oopsss... Error`)
+}
+// With RegExp
+zip = prompt(`Enter zip-code of 5 numbers`);
+zipLength = zip.length;
+zip = +zip;
+ if (zipLength === 5 && !isNaN(zip) && zip) {
+    alert(`Your zip-code was successfully received`)
+} else {
+    alert(`oopsss... Error`)
+}
