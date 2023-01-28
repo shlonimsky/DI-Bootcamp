dfa0fb185fe29b286b490b2517212c3720fd1ce5
shlonimsky <ashlonimsky@gmail.com>
2023-01-28 13:13:56 +0200

    day 5 exGold is done without bonus

--- Week2/Day5/ExercisesXPGold/script.js
+++ Week2/Day5/ExercisesXPGold/script.js
@@ -6,14 +6,51 @@
 // Hint : you can use the eval() method to help with your calculations.
 // 3. Before coding, take your time to understand all the parts to the exercise and their process. You can write it down somewhere if it helps (recommended).
 // 4. Bonus : create the RESET and CLEAR buttons.
+let expression = "";
+let numberString ="";
+let numb = 0;
+let expCalc = [];
+
 function number(num){
-return num
+numberString += num;
+expression += num;
+// console.log(typeof expression)
+console.log(expression)
 }
+
 function operator(operator){
-    console.log(operator)
+    numb=+numberString;
+    expCalc.push(numb)
+    expCalc.push(operator)
+    expression += operator
+    console.log(expression)
+    numberString = "";
 }
+
+
 function equal(){
-    let equal = number(num)
+    let result;
+    numb=+numberString;
+    expCalc.push(numb)
+    switch (expCalc[1]){
+        case '+' : result = expCalc[0]+expCalc[2]
+        break
+        case "-" : result = expCalc[0]-expCalc[2]
+        break
+        case "/" : result = expCalc[0]/expCalc[2]
+        break
+        case "*" : result = expCalc[0]*expCalc[2]
+        break
+        default : console.log(`error`)
+    }
+    expression += "="+result;
+console.log(expression)
 }
-reset()
-clear()
+// reset()
+// function clear(){
+//     console.log(`Clear`)
+//      expression = "";
+//      numberString ="";
+//      numb = 0;
+//  expCalc = [];
+// }
