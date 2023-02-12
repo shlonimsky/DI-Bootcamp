e02e86d31fe9523315a2a7477077ac53e7d0e0b5
shlonimsky <ashlonimsky@gmail.com>
2023-02-12 13:39:28 +0200

    is done

---
+++ Week5/Day1/ExercisesXP/script.js
@@ -0,0 +1,48 @@
+// 🌟 Exercise 1 : HTML Form
+// Create a form like the one above. The form should contain three inputs:
+// a small text input asking for a name,
+// a larger textarea input to write a message,
+// a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+// Where will the sent data appear?
+// at the top in the url field: index.html?username=Anastasiia&comment=Comments
+
+// 🌟 Exercise 2 : HTML Form #2
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+// With the POST method it will appear in Network Tab, in the Playload Tab will be shown the Form Data
+
+// 🌟 Exercise 3 : JSON Mario
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+//   Convert this JS object into a JSON object. What happens to the nested objects ?
+const marioJson = JSON.stringify(marioGame)
+console.log(marioJson);
+// Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+const marioJsonPretty = JSON.stringify(marioGame,null,3)
+console.log(marioJsonPretty);
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
