f845ca9a400c9d227681371028816b944df6e17a
shlonimsky <ashlonimsky@gmail.com>
2023-02-26 13:57:08 +0200

    xp is done

---
+++ Week7/Day1/ExercisesXP/npm-node/main.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 10;
+// Write some more code in the main.js file so when you run it in the command prompt it logs Hello you are 15
+console.log(`Hello, you are ${a+b}`)
+console.log('fire chicken rules')
+
+const a1 = 5;
+const b1 = 10;
+let x;
+const c = 6;
+x = a1 + b1;
+console.log(x)
+console.log(x+c)
