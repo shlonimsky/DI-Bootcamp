69a1f7f813c56f24a8cfddd40ed9564c24de91a3
shlonimsky <ashlonimsky@gmail.com>
2023-02-27 09:44:04 +0200

    W7D1

---
+++ Week7/Day1/ExercisesXPNinja/script.js
@@ -0,0 +1,5 @@
+const faker = require('./faker')
+let users =[]
+const randomName = faker.name.fullName(); // Rowan Nikolaus
+// const randomEmail = faker.internet.email();
+console.log("faker",faker)
