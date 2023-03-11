a793598a0c51bd2adec33b0bc8328951fa44bbc3
shlonimsky <ashlonimsky@gmail.com>
2023-03-11 20:18:48 +0200

    server installing

--- Week8/Day4/ex-xp-ninja/src/App.js
+++ Week8/Day4/ex-xp-ninja/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+// Exercise 1: Get JSON Data From Your Express Backend
+// You will get JSON Data from your Express Backend and output the result with React JS. Make sure to use the state to hold the data used in your app.
 
+// Create your Backend with Express.
+// (OPTIONAL) You can install the Express Generator package to create your server in a faster way.
+
+// Create you backend folder with the command: express react-backend
+
+// cd into your folder and run npm install
+
+// Open the users.js file, and delete the send response in the get router. Instead add the below response json object, and save the file.
+
+//     res.json([
+//                {id: 1, username: "somebody"},
+//                {id: 2, username: "somebody_else"},
+//             ]);
+
+
+// cd to users.js and run your Backend with PORT=3001 - npm start (React App uses 3000, so we don’t want any conflicts)
+
+
+// For windows Users: run PORT=3001 && node bin/www
+
+// Open http://localhost:3001/users in your browser you will see that the json data is in your backend.
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
