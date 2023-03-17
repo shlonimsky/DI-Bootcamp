769d8b0730fa49b041c6331f992f70e7195a4302
shlonimsky <ashlonimsky@gmail.com>
2023-03-18 00:30:39 +0200

    using Hooks

--- Week9/Day5/ex-redux-up/src/Components/Home.js
+++ Week9/Day5/ex-redux-up/src/Components/Home.js
@@ -1,17 +1,19 @@
 import logo from '../blog.png'
 import { Link } from 'react-router-dom'
 import { useState,useEffect} from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+// import { connect } from 'react-redux'
 
 
 const Home = (props) => {
 
 const [posts, setPost] = useState([])
 
+const postsRedux = useSelector((state) => state.data)
 useEffect(() => {
-setPost(props.posts)
-// console.log(posts)
-},[posts])
+    setPost(postsRedux)
+    // console.log(posts)
+    },[postsRedux])
 
 return posts.length===0 ? (<p>No post to show</p>) : (
     <div className='container home'>
@@ -35,10 +37,11 @@ return posts.length===0 ? (<p>No post to show</p>) : (
 }
 
 
-const mapStateToProps = (state) => {
-    return {
-        posts : state.data
-    }
-}
+// const mapStateToProps = (state) => {
+//     return {
+//         posts : state.data
+//     }
+// }
 
-export default connect(mapStateToProps)(Home)
+// export default connect(mapStateToProps)(Home)
+export default Home
