4bbe60984de967704744a4c6736ba123bf586258
shlonimsky <ashlonimsky@gmail.com>
2023-03-19 17:16:57 +0200

    done

--- Week10/Day1_hooks_deployment/google-map/src/App.js
+++ Week10/Day1_hooks_deployment/google-map/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
+const ApiKey = 'AIzaSyAnvjQD1GtUzZDb9RN9BB1MtHnHgo3_Fjo'
 
 function App() {
+  console.log(GoogleMapReact)
+    const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', width: '100%' }}> 
+    <GoogleMapReact  bootstrapURLKeys={{ key: ApiKey }}
+                      defaultCenter={defaultProps.center}
+                      defaultZoom={defaultProps.zoom}>
+
+    </GoogleMapReact>
     </div>
   );
 }
